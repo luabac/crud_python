6e8de298ad786e8454d3d6c6e4ea85b72d2686ac
Luã Botelho <luabachayb@gmail.com>
2026-03-24 21:17:03 -0300

    feat: Operação de CRUD para usar consultas parametrizadas e remover código comentado.

--- CRUD PYTHON/aula.py
+++ CRUD PYTHON/aula.py
@@ -12,60 +12,34 @@ cursor = conexao.cursor()
 #---------CRUD---------
 
 
-# CREATE
-# nome_produto = "balinha"
-# valor = 7.5
-# comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
-# cursor.execute(comando)
-# conexao.commit() # edita o banco de dados
+CREATE
+nome_produto = "balinha"
+valor = 7.5
+comando = 'INSERT INTO vendas (nome_produto, valor) VALUES (%s, %s)'
+cursor.execute(comando, (nome_produto, valor))
+conexao.commit() # edita o banco de dados
 
 
-# READ
-# comando = f'SELECT * FROM vendas'
-# cursor.execute(comando) #cursor é quem executa os comandos SQL dentro dessa conexão
-# resultado = cursor.fetchall() # ler o banco de dados
-# print(resultado)
+READ
+comando = 'SELECT * FROM vendas'
+cursor.execute(comando) #cursor é quem executa os comandos SQL dentro dessa conexão
+resultado = cursor.fetchall() # ler o banco de dados
+for linha in resultado:
+    print(linha)
 
-# UPDATE
-# nome_produto = "chocolate"
-# valor = 15
-# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
-# cursor.execute(comando)
-# conexao.commit() # edita o banco de dados
+UPDATE
+nome_produto = "chocolate"
+valor = 15
+comando = 'UPDATE vendas SET valor = %s WHERE nome_produto = %s'
+cursor.execute(comando, (valor, nome_produto))
+conexao.commit() # edita o banco de dados
 
-# DELETE
+DELETE
 nome_produto = "chocolate"
 valor = 9
-comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
-cursor.execute(comando)
+comando = 'DELETE FROM vendas WHERE nome_produto = %s'
+cursor.execute(comando, (nome_produto))
 conexao.commit() # edita o banco de dados
 
 cursor.close()
 conexao.close()
-
-
-
-
-
-# import mysql.connector
-
-# conexao = mysql.connector.connect(
-#     host='localhost',
-#     user='root',
-#     password='msn@Root1',
-#     database=''
-# )
-
-# cursor = conexao.cursor()
-
-# nome_produto = ""
-# valor = ""
-# comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", valor)'
-# cursor.execute(comando)
-# conexao.commit()
-
-
-
-# comando
-# cursor.execute(comando)
-# conexao.commit()
